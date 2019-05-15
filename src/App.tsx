a6d2639058950cdd1054a321ba733db97504168d
Onyeka Obi <oobi@BELC02Q6GHWG8WN.sea.corp.expecn.com>
2019-05-15 01:49:30 -0700

    Refactor to functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Link: React.FC<{text : string }> = ({text}) => (
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {text}
+    </a>
+)
+
+const Paragraph: React.FC<{path: string, action: string}> = ({path, action}) => (
+  <p>
+    Edit <code>{path}</code> {action}
+  </p>
+)
+
+const Image: React.FC<{logo: string, className: string, alt: string}> =
+  ({logo, className, alt}) => (
+    <img src={logo} className={className} alt={alt} />
+  )
+
+const Header: React.FC<{className: string, children: ReactElement[]}> = ({className, children}) => (
+    <header className={className} style={{
+      "background-color": "#282c34",
+      "min-height": "100vh",
+      "display": "flex",
+      "flex-direction": "column",
+      "align-items": "center",
+      "justify-content": "center",
+      "font-size": "calc(10px + 2vmin)",
+      "color": "white"
+    }}>
+      {children}
+    </header>
+)
+
+const Div: React.FC<{className: string, children: ReactElement[]}> = ({className, children}) => (
+  <div className={className}>
+    {children}
+  </div>
+)
+
+const headerChildren: ReactElement[] = [
+  <Image logo={logo} className="App-logo" alt="logo" />,
+  <Paragraph path="src/App.tsx" action="and save to reload." />,
+  <Link text="Learn React" />
+]
+
+const App: React.FC = () => (
+    <Div className="App" children={[
+      <Header className="App" children={headerChildren} />
+    ]} />
+  )
 
 export default App;
